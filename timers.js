541ddebdbbf156bfa5fb9b5ef219f2772effb4cb
Dmitry <ru.goodbye@gmail.com>
2016-02-19 15:33:07 +1100

    Improve the script

--- timers.js
+++ timers.js
@@ -1,80 +1,107 @@
 jQuery(function($){
+	var cellActive = function(el) {
+		el.parent().css('background-color', '#D49B90');
+	};
 
-    var faucets = [];
-    var addFaucet = function( index, cell, seconds ) {
-        faucets[index] = {state: 0, period: seconds, cell: cell};
-    };
+	var cellInactive = function(el) {
+		el.parent().css('background-color', 'rgba(0,0,0,0.02)');
+	};
 
-    var startFaucetTimer = function( index ) {
-        var faucet = faucets[index];
-        if( faucet.state != 1 ) {
-            faucet.state = 1;
-            faucet.left = faucet.period;
-            refreshCellTimer( index );
-            faucet.cell.css('background', '#D49B90');
-        }
-    };
 
-    var stopFaucetTimer = function( index ) {
-        var faucet = faucets[index];
-        faucet.state = 0;
-        faucet.cell.html( faucet.period / 60 + '&nbsp;мин.' );
-        faucet.cell.css('background', '#A7D490');
-    }
+	var faucets = [];
 
-    var refreshCellTimer = function( index ) {
-        var faucet = faucets[index];
-        faucet.cell.html( faucet.left + '&nbsp;с.' );
-    };
+	var addFaucet = function( index, cell, seconds, measure ) {
+	    faucets[index] = {state: 0, period: seconds, cell: cell, measure: measure};
+	    console.log("Add: " + index + ", " + seconds + ", measure = ", measure)
+	};
 
-    var checkFaucets = function() {
-        for( i = 0; i<faucets.length ; i++ ) {
-            var faucet = faucets[i];
-            if( !faucet ) continue;
-            if( faucet.state == null ) {
-                continue;
-            } else if( faucet.state == 1 ) {
-                faucets[i].left--;
-                refreshCellTimer( i );
-                if( faucets[i].left == 0 ) {
-                    stopFaucetTimer(i);
-                }
-            }
-        }
-    };
+	var startFaucetTimer = function( index ) {
+	    var faucet = faucets[index];
+	    console.log("Start timer for " + index + ". State = " + faucet.state)
+	    if( faucet.state != 1 ) {
+	        faucet.state = 1;
+	        faucet.left = faucet.period;
+	        refreshCellTimer( index );
+	        cellActive(faucet.cell);
+	    }
+	};
 
-    var startGlobalTimer = function() {
-        setInterval( function(){
-            checkFaucets();
-        }, 1000);
-    };
+	var stopFaucetTimer = function( index ) {
+	    var faucet = faucets[index];
+	    faucet.state = 0;
 
-    var initCellTimer = function( row, cell, index ) {
-        var link = $(row.find('td').get(0));
-        link.find('a');
-        link.click(function(){
-            startFaucetTimer(index);
-        });
-    }
+	    if( faucet.measure == 'min' ) {
+	    	faucet.cell.html( faucet.period / 60 + '&nbsp;мин' );
+	    } else if ( faucet.measure == 'sec' ) {
+	    	faucet.cell.html( faucet.period + '&nbsp;сек' );
+	    }
 
-    var initFaucets = function(rows) {
-        var searchRe = /(\d+)\s+мин/i;
+	    cellInactive(faucet.cell);
+	}
 
-        rows.each(function(i, row){
-            row = $(row);
-            var cells = row.find('td');
-            var periodCell = $(cells.get(2));
-            var result = searchRe.exec(periodCell.text());
+	var refreshCellTimer = function( index ) {
+	    var faucet = faucets[index];
+	    faucet.cell.html( faucet.left + '&nbsp;с.' );
+	};
 
-            if( result != null ) {
-                var minutes = result[1];
-                addFaucet( i, periodCell, minutes*60 );
-                initCellTimer( row, periodCell, i );
-            }
-        });
-    };
+	var checkFaucets = function() {
+	    for( i = 0; i<faucets.length ; i++ ) {
+	    	var faucet = faucets[i];
+	    	if( !faucet ) continue;
+	    	if( faucet.state == null ) {
+	    		continue;
+	    	} else if( faucet.state == 1 ) {
+	    		faucets[i].left--;
+	    		refreshCellTimer( i );
+	    		if( faucets[i].left == 0 ) {
+	    			stopFaucetTimer(i);
+	    		}
+	    	}
+	    }
+	};
 
-    var rows = $('table').find('tr');
-    initFaucets( rows );
-    startGlobalTimer();
+	var startGlobalTimer = function() {
+		setInterval( function(){
+			checkFaucets();
+		}, 1000);
+	};
+
+	var initCellTimer = function( row, cell, index ) {
+		var link = $(row.find('td').get(0)).find('a');
+		link.click(function(){
+			startFaucetTimer(index);
+		});
+	}
+
+	var initFaucets = function(rows) {
+		var searchReMin = /^\s*(\d+)\s+мин/i;
+		var searchReSec = /^\s*(\d+)\s+сек/i;
+
+		var counter = 0;
+		rows.each(function(i, row){
+			counter++;
+			row = $(row);
+			var cells = row.find('td');
+			var periodCell = $(cells.get(2)).find('span');
+			var result = searchReMin.exec(periodCell.text());
+			if( result != null ) {
+				var minutes = result[1];
+				addFaucet( i, periodCell, minutes*60, 'min' );
+				initCellTimer( row, periodCell, i );
+			} else {
+				var result = searchReSec.exec(periodCell.text());
+				if( result != null ) {
+					var seconds = result[1];
+					addFaucet( i, periodCell, seconds, 'sec' );
+					initCellTimer( row, periodCell, i );
+				}
+			}
+		});
+
+		console.log("Init " + counter + " faucets.")
+	};
+
+	var rows = $('table').find('tr');
+	initFaucets( rows );
+	startGlobalTimer();
 });
